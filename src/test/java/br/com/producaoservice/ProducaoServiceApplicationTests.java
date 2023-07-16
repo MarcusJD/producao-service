4693e9c8e9df21a6a40259ac3ac6017630659e4c
Marcus <flambres3@gmail.com>
2023-07-16 18:29:35 -0300

    Commit do projeto.

---
+++ src/test/java/br/com/producaoservice/ProducaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.producaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
