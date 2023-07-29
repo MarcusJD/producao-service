09b1a739fa519a105bf08560518d9a69ea627d3c
Marcus <flambres3@gmail.com>
2023-07-29 20:17:25 -0300

    Annotation da entidade unidade de medida

--- src/main/java/br/com/producaoservice/model/UnidadeMedidaEntity.java
+++ src/main/java/br/com/producaoservice/model/UnidadeMedidaEntity.java
@@ -1,12 +1,25 @@
 package br.com.producaoservice.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigInteger;
 
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "TB_UNIDADE_MEDIDA")
 public class UnidadeMedidaEntity {
 
     @Id
